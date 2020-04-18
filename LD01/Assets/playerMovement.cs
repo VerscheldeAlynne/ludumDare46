cb17f9ab221534e36218e5af10907872fd4014d8
alynne <flore.verschelde@student.vives.be>
2020-04-18 11:36:14 +0200

    gravity added

--- LD01/Assets/playerMovement.cs
+++ LD01/Assets/playerMovement.cs
@@ -5,16 +5,36 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
 
-    public float moveSpeed = 12f;
     public CharacterController controller;
+
+
+    public float moveSpeed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = .4f;
+    public LayerMask groundMask;
 
     float x;
     float z;
 
     Vector3 move;
+    Vector3 velocity;
+    bool isGrounded;
+
+
+
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         //input
        x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
@@ -26,5 +46,9 @@ public class playerMovement : MonoBehaviour
         move = new Vector3(x, 0f, z);
 
         controller.Move(move *moveSpeed*Time.deltaTime);
+
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
