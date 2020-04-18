c167e35c516bac4d036333c836bfe4d36617be37
alynne <flore.verschelde@student.vives.be>
2020-04-18 11:00:11 +0200

    player added

---
+++ LD01/Assets/playerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 12f;
+    public CharacterController controller;
+
+    float x;
+    float z;
+
+    Vector3 move;
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+       x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //movement
+        move = new Vector3(x, 0f, z);
+
+        controller.Move(move *moveSpeed*Time.deltaTime);
+    }
+}
