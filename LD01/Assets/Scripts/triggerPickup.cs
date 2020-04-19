78633d1b184febeec1ec992840027a776e13edce
alynne <flore.verschelde@student.vives.be>
2020-04-19 08:45:34 +0200

    emmer oprapen

---
+++ LD01/Assets/Scripts/triggerPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerPickup : MonoBehaviour
+{
+
+    public Transform tool;
+    public Transform destination;
+
+    private void OnTriggerStay (Collider other)
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            Debug.Log("pickup");
+            tool.transform.position = destination.position;
+            tool.transform.parent = GameObject.Find("toolDestination").transform;
+        }
+            
+    }
+
+   
+}
