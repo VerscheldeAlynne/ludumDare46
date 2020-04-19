a0d704414eeaf65b8fe1b0bbf5855a075964b005
nathanve <56344181+nathanve@users.noreply.github.com>
2020-04-19 23:50:56 +0200

    Fix van vorige commits, hopelij alles terug nu (screens prefab, models, etc)

---
+++ LD01/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public GameObject[] Screens;
+    // Start is called before the first frame update
+
+    public bool gameStarted;
+    void Start()
+    {
+        Screens[0].SetActive(true);
+        gameStarted = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey && gameStarted == false)
+        {
+            Screens[0].SetActive(false);
+            Debug.Log("A key or mouse click has been detected");
+            gameStarted = true;
+        }
+
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
